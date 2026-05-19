8323129647164a5347f331b406c91b7b744edc1e
MatHoyer <mathieuhoyer@proton.me>
2026-05-19 19:10:52 +0200

    first shot

--- src/components/ui/loader.tsx
+++
@@ -1,6 +0,0 @@
-import { cn } from '@/lib/utils/utils';
-import { Loader2, LucideProps } from 'lucide-react';
-
-export const Loader: React.FC<LucideProps> = ({ className, ...props }) => {
-  return <Loader2 className={cn(className, 'animate-spin')} {...props} />;
-};
--- src/features/items/schemas/check-item.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const checkItemSchema = z.object({
-  id: z.string().cuid(),
-});
-
-export type TCheckItemSchema = z.infer<typeof checkItemSchema>;
